4ff29c9c88a00c953ef34381c0a119e88b642773
Akshay Kumar <taara0361@gmail.com>
2026-05-31 01:58:19 +0530

    deployment bug fix

--- src/server.ts
+++ src/server.ts
@@ -4,5 +4,5 @@ import { env } from "./config/env";
 const PORT = env.PORT;
 
 app.listen(PORT, () => {
-  console.log(`Chitralai backend running on http://localhost:${PORT}`);
+  console.log(`Chitralai backend running on https://chitralaitask.onrender.com`);
 });
